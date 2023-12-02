cfddfdffe7b451fb446a794efd15bf644b944c16
NandaScott <nanda1123@gmail.com>
2023-12-02 13:41:28 -0500

    Finished 2023 day 2

---
+++ 2023/02/02.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from 'bun:test';
+import main, { addGameIds, byMaximum, byMinimum, addPowers } from './02';
+
+const exampleInput = [
+  'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+  'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+  'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+  'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+  'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+]
+
+const puzzle = Bun.file('./2023/02/02.input.txt');
+const input = (await puzzle.text()).split(/\n/);
+
+describe.only('Advent Day 2', () => {
+  it('should solve example 1', () => {
+    const output = main(exampleInput, byMaximum, addGameIds);
+    expect(output).toBe(8);
+  })
+
+  it('should solve puzzle 1', () => {
+    const output = main(input, byMaximum, addGameIds);
+    expect(output).toBe(2006);
+  })
+
+  it('should solve example 2', () => {
+    const output = main(exampleInput, byMinimum, addPowers);
+    expect(output).toBe(2286);
+  })
+
+  it('should solve puzzle 2', () => {
+    const output = main(input, byMinimum, addPowers);
+    expect(output).toBe(84911);
+  })
+})
---
+++ 2023/02/02.ts
@@ -0,0 +1,67 @@
+type GameResult = [blue: number[], green: number[], red: number[]];
+
+type GameRecord = [gameNumber: number, results: GameResult];
+
+type FilterFunction = (game: GameRecord) => GameRecord | undefined;
+
+type MathFunction = (games: GameRecord[]) => number
+
+const getCubes = (game: string, regex: RegExp) =>
+  Array.from(game.matchAll(regex), (match) => parseInt(match[2]))
+
+function parseGames(arg: string[]): GameRecord[] {
+  return arg.map((game, i) => {
+    const gameNo = i + 1;
+    const blue = getCubes(game, /((\d{1,})\sblue)/gm);
+    const green = getCubes(game, /((\d{1,})\sgreen)/gm);
+    const red = getCubes(game, /((\d{1,})\sred)/gm);
+    return [gameNo, [blue, green, red]]
+  });
+}
+
+export function byMaximum(game: GameRecord): GameRecord | undefined {
+  const [blue, green, red] = game[1];
+  const MAX_BLUE = 14;
+  const MAX_GREEN = 13;
+  const MAX_RED = 12;
+
+  const blueValid = blue.every((num) => num <= MAX_BLUE);
+  const greenValid = green.every((num) => num <= MAX_GREEN);
+  const redValid = red.every((num) => num <= MAX_RED);
+
+  if (blueValid && greenValid && redValid) {
+    return game;
+  }
+}
+
+export function byMinimum(game: GameRecord): GameRecord | undefined {
+  const [blue, green, red] = game[1];
+  const blueMin = [Math.max(...blue)];
+  const greenMin = [Math.max(...green)];
+  const redMin = [Math.max(...red)];
+  game[1] = [blueMin, greenMin, redMin]
+  return game;
+}
+
+export function addGameIds(games: GameRecord[]): number {
+  return games.reduce((prev, curr) => (prev + curr[0]), 0)
+}
+
+export function addPowers(games: GameRecord[]): number {
+  const multiplier = (a: number, b: number) => a * b;
+  return games.reduce((prev, curr) => {
+    const [blue, green, red] = curr[1];
+    const bluePower = blue.reduce(multiplier);
+    const greenPower = green.reduce(multiplier);
+    const redPower = red.reduce(multiplier);
+    const totalPower = bluePower * greenPower * redPower;
+    return prev + totalPower;
+  }, 0);
+}
+
+export default function main(input: string[], filterBy: FilterFunction, mathFunction: MathFunction): number {
+  const gameRecords = parseGames(input);
+  const validGames = gameRecords.filter(filterBy);
+  const validGamesTotal = mathFunction(validGames);
+  return validGamesTotal;
+}
